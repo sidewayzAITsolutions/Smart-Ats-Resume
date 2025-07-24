25c2fa8ff72797f88fe5d9144f8fb25f8fa433bb
William Benjamin Shirley <benjamin@buttond.com>
2025-07-23 22:51:33 -0500

    Delete stripe.ts

--- stripe.ts
+++
@@ -1,9 +0,0 @@
-// lib/stripe.ts
-import Stripe from 'stripe';
-
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-10-16',
-  typescript: true,
-});
-
-export default stripe;
