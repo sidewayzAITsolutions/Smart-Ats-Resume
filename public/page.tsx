2ed40339026b61e6369951853773ce5ebc2a16cc
William Benjamin Shirley <buttondinc@gmail.com>
2025-07-27 19:57:02 -0500

    feat: add GlobalNavigation component import to page.tsx

--- public/page.tsx
+++ public/page.tsx
@@ -53,6 +53,7 @@ import { useSearchParams } from 'next/navigation';
 import toast from 'react-hot-toast';
 
 import FloatingATSGuide from '@/components/FloatingATSGuide';
+import GlobalNavigation from '@/components/GlobalNavigation';
 import UserDropdown from '@/components/UserDropdown';
 import { createClient } from '@/lib/supabase/client';
 
